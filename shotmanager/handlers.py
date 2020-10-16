0109df8b28a6b7b9c0f45904fe05222fcfa65c66
Romain <romain.carriquiry-borchiari@ubisoft.com>
2020-10-16 15:49:59 +0200

    backward playing

--- shotmanager/handlers.py
+++ shotmanager/handlers.py
@@ -1,131 +1,76 @@
 import bpy
 
-
+previous_frame = 0
 def jump_to_shot(scene):
-
+    props = scene.UAS_shot_manager_props
     verbose = False
 
-    def getPreviousShotIndex(shots, current_index):
-        previous_index = (current_shot_index - 1) % len(shots)
-        while current_index != previous_index:
-            if shots[previous_index].enabled:
-                return previous_index
-            previous_index = (previous_index - 1) % len(shots)
+    def get_previous_shot ( shots, current_shot ):
+        index = props.getShotIndex ( current_shot )
+        if index > 0:
+            previous_shots = [ s for s in shots[ : index ] if s.enabled ]
+            if len ( previous_shots ):
+                return  previous_shots[ -1 ]
 
-        return -1
+        return None
 
-    def getNextShotIndex(shots, current_index):
-        next_index = (current_shot_index + 1) % len(shots)
-        while current_index != next_index:
-            if shots[next_index].enabled:
-                return next_index
-            next_index = (next_index + 1) % len(shots)
 
-        return -1
+    def get_next_shot (shots, current_shot ):
+        index = props.getShotIndex ( current_shot )
+        if index < len ( shots ) -1:
+            next_shots = [ s for s in shots[ index + 1: ] if s.enabled ]
+            if len ( next_shots ):
+                return next_shots[ 0 ]
 
-    def getFirstShotFrameIfEndReached(currentFrame):
-        """ Return an array with next frame and next shot index.
-            Next frame is current frame if no frame is found
-            Next shot index is -1 if not found
-        """
-        # print("   getFirstShotFrameIfEndReached, currentFrame: ", currentFrame)
-        frameAndShotToUse = [currentFrame, -1]
-        firstShotIndContainingFrame = -1
+        return None
 
-        if (scene.use_preview_range and scene.frame_preview_end <= currentFrame) or (
-            not scene.use_preview_range and scene.frame_end <= currentFrame
-        ):
-            if verbose:
-                print("        *** getFirstShotFrameIfEndReached Current frame at end of range : ", currentFrame)
-            startRangeFrame = scene.frame_preview_start if scene.use_preview_range else scene.frame_start
-            if verbose:
-                print("        startRangeFrame: ", startRangeFrame)
-            firstShotIndContainingFrame = props.getFirstShotIndexContainingFrame(startRangeFrame, ignoreDisabled=True)
-
-            # if shot found
-            if -1 != firstShotIndContainingFrame:
-                if verbose:
-                    print("       firstShotIndContainingFrame for start range frame: ", firstShotIndContainingFrame)
-                frameAndShotToUse = [startRangeFrame, firstShotIndContainingFrame]
-            else:
-                if verbose:
-                    print("       No firstShotIndContainingFrame for frame: ", startRangeFrame)
-                firstShotIndAfterFrame = props.getFirstShotIndexAfterFrame(startRangeFrame, ignoreDisabled=True)
-                if -1 != firstShotIndAfterFrame:
-                    endRangeFrame = scene.frame_preview_end if scene.use_preview_range else scene.frame_end
-                    if shotList[firstShotIndAfterFrame].start > endRangeFrame:
-                        # found shot is out of range
-                        frameAndShotToUse = [currentFrame, -1]
-                    else:
-                        frameAndShotToUse = [shotList[firstShotIndAfterFrame].start, firstShotIndAfterFrame]
-
-                    if verbose:
-                        print("       frameAndShotToUse for shot after: ", frameAndShotToUse)
-                else:
-                    frameAndShotToUse = [currentFrame, -1]
-
-                # if firstShotIndAfterFrame trop grand...
-            # wkip warning: firstShotIndContainingFrame peut etre -1 car pas de shot sous cette frame. Prendre alors le premier shot qui vient après
-
-        # else:
-        #     print(" *** Not End frame: ", scene.frame_current)
-        #     firstRangeFrame = shotList[current_shot_index].start
-
-        # -1 mal choisit!!!!
-
-        return frameAndShotToUse
-
-    props = scene.UAS_shot_manager_props
+    global previous_frame
     shotList = props.get_shots()
     current_shot_index = props.current_shot_index
-    old_shot_index = current_shot_index
     props.restartPlay = False
 
-    # if scene.frame_end == scene.frame_current or scene.frame_preview_end == scene.frame_current:
-    #     print(" *** *** End of range reached: ", scene.frame_current)
-    #     print("\n")
+    current_shot = shotList[ current_shot_index ]
     current_frame = scene.frame_current
-    current_shot_end = shotList[ current_shot_index ].end
 
+    # clip shot to scene timeframe. Might not be necessary
+    current_shot_start = current_shot.start
+    current_shot_end = current_shot.end
 
-    if bpy.context.screen.is_animation_playing:
-        ##   print(" *** Playing - after return, frame: ", scene.frame_current)
-        print ( shotList[  current_shot_index ].name )
-        if not shotList[current_shot_index].start < current_frame < current_shot_end:
-            if current_frame > current_shot_end:
-                disp = current_frame - current_shot_end
-                next_shot_index = getNextShotIndex ( shotList, current_shot_index )
-                while next_shot_index != -1:
-                    shot = shotList[ next_shot_index ]
-                    if disp < shot.getDuration ( ):
-                        current_shot_index = next_shot_index
-                        props.setCurrentShot ( shot )
-                        scene.frame_current = shot.start + disp
-                        break
+    scene_frame_start = scene.frame_preview_start if scene.use_preview_range else scene.frame_start
+    scene_frame_end = scene.frame_preview_end if scene.use_preview_range else scene.frame_end
 
-                    disp -= shot.getDuration ( )
-                    next_shot_index = getNextShotIndex ( shotList, next_shot_index )
-                else:
-                    pass #end of timeline
-    # not playing
+    if not bpy.context.screen.is_animation_playing:
+        return
+
+    if not bpy.context.screen.is_scrubbing:
+        if current_frame == scene_frame_end and get_previous_shot ( shotList, current_shot ) is None:
+            props.setCurrentShot ( shotList[ -1 ] )
+            scene.frame_current = shotList[ -1 ].end
+        elif current_frame > current_shot_end:
+            disp = current_frame - current_shot_end
+            next_shot = get_next_shot  ( shotList, current_shot )
+            while next_shot is not None:
+                if disp < next_shot.getDuration ( ):
+                    props.setCurrentShot ( next_shot )
+                    scene.frame_current = next_shot.start + disp
+                    break
+                disp -= next_shot.getDuration ( )
+                next_shot = get_next_shot  ( shotList, next_shot )
+        elif current_frame == scene_frame_start and get_next_shot  ( shotList, current_shot ) is None: # Loop back at start.
+            # Seems that the first frame is always hit even in frame dropping playblack
+            props.setCurrentShot ( shotList[ 0 ] )
+        elif current_frame < current_shot_start:
+            disp = current_shot_start - current_frame
+            previous_shot = get_previous_shot ( shotList, current_shot )
+            while previous_shot is not None:
+                if disp < previous_shot.getDuration ( ):
+                    props.setCurrentShot ( previous_shot )
+                    scene.frame_current = previous_shot.end - disp
+                    break
+                disp -= previous_shot.getDuration ( )
+                previous_shot = get_next_shot ( shotList, previous_shot )
+
     else:
-        if verbose:
-            print(" ùùù NOT Playing - after return, frame: ", scene.frame_current)
-        # having a separation between playing and not playing is very important to let the
-        # shot manager playbar buttons work. They work without the time range restrictions.
+        pass
 
-    if old_shot_index != current_shot_index:
-        if -1 < current_shot_index and shotList[current_shot_index].camera is not None:
-            if scene.camera != shotList[current_shot_index].camera:
-                scene.camera = shotList[current_shot_index].camera
-            props.selected_shot_index = current_shot_index
-
-
-# def jump_to_shot__frame_change_post(self):
-#     scene = bpy.context.scene
-#     props = scene.UAS_shot_manager_props
-#     print("jump_to_shot__frame_change_post, fr: ", scene.frame_current)
-#     if props.restartPlay:
-#         props.restartPlay = False
-#         #  bpy.app.handlers.frame_change_pre.append(jump_to_shot)
-#         pass
+    previous_frame = scene.frame_current
