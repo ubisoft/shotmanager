0c5386e536545b5bc81feaea379d294412b8f58d
julien blervaque <julien.blervaque@ubisoft.com>
2021-03-22 17:23:48 +0100

    del temp tools

--- tools/WkWriteFileHeader/main.py
+++
@@ -1,7 +0,0 @@
-from wkwritefileheader import prepend_text_to_file
-
-
-# dir = input("Enter a directory: ")
-dir = "Z:/EvalSofts/Blender/DevPython/UAS_ShotManager_Addon/shotmanager/"
-prepend_text_to_file(dir, "py", "tools\WkWriteFileHeader\gnu_licence.txt")
-
